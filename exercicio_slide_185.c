568cbe58aa057b8649819daca35c9af184c62d7d
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-09-03 21:31:43 -0300

    Create exercicio_slide_185.c
    Exercício do slide 185
    
    Mostrar os 10 primeiros números pares, a posição deles e a soma.

---
+++ exercicio_slide_185.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<locale.h>
+
+int main(){
+    setlocale(LC_ALL, "");
+
+    int n = 1, contador = 0, soma = 0;
+
+    while(contador < 10){
+        soma += n;
+        printf("\n%dº ímpar: %d -- Soma = %d\n", contador + 1, n, soma);
+        n += 2;
+        contador++;
+
+    }
+}
