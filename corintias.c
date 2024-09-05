d7d2023d06912677e63a89d6f2b7b0892127295c
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-09-04 21:39:41 -0300

    Create corintias.c

---
+++ corintias.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+
+int main(){
+    setlocale(LC_ALL, "");
+int i = 1;
+while (i > 0){
+
+    printf("                  mm\n");
+    printf("                ##  ##\n");
+    printf("                ##  ..\n");
+    printf("  ##            MM##::            ##\n");
+    printf("######        ##########        ######\n");
+    printf("########      @@######..      MM######\n");
+    printf("######      ..    --::    MM##mm##\n");
+    printf("##  ::  ++##::  ####..mm##  ##\n");
+    printf("       mm@@    ####MM@@@@####\n");
+    printf("        ::####          ##  @@\n");
+    printf("           ++    ##::  ##  ##..@@\n");
+    printf("      ::##  ##  ####mm##      ##\n");
+    printf("    mm##    ..    ####  ##  ####@@\n");
+    printf("    ####      mm  ####  ##  ######\n");
+    printf("    ..####      ..@@    ##  ####\n");
+    printf("        ##@@  @@##..##  ##::@@\n");
+    printf("##      ##          ##    ::####    mm\n");
+    printf("##::    ##  ######    ########  MM--##\n");
+    printf("mm##    mm########  ##  ####      ----\n");
+    printf("  ####      MMMM  --..mm##  @@  ####\n");
+    printf("      ##                      ##MM\n");
+    printf("    mm  ##      --##        ##MM\n");
+    printf("  ::    ##########MM######mm##\n");
+    printf("          ++##++  MM  ####..\n");
+    printf("                ##MM##\n");
+    printf("                  ##\n");
+}
+}
