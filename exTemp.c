cdbf81b1ca7b5abdef2ca7dcb25ded639bbe7743
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-09-06 21:15:07 -0300

    Update exTemp.c

--- exTemp.c
+++ exTemp.c
@@ -6,40 +6,58 @@ int main(){
     setlocale(LC_ALL, "");
 
     float temp, C, F, K;
-    char escolha;
+    int escolha, continuar = 1;
 
-    printf("\nDigite o valor da temperatura sem a letra: ");
-    scanf("%f", &temp);
-    fflush(stdin);
-    printf("\nEscolha a escala: ");
-    escolha = getchar();
+    do{
+        printf("\nDigite o valor da temperatura(somente números): ");
+        scanf("%f", &temp);
+        fflush(stdin);
+        printf("\nEscolha a escala: ");
+        printf("\n(1) - Celsius \n(2) - Fahrenheit \n(3) - Kelvin\n\n");
+        //escolha = getchar();
+        scanf("%d", &escolha);
 
-    switch(escolha){
-        case 'c':
-            C = temp;
+        switch(escolha){
+            case 1:
+                C = temp;
 
-            F = (9 * C + 160) / 5;
-            K = C + 273;
+                F = (9 * C + 160) / 5;
+                K = C + 273;
 
-            printf("\n%.2fºC = %.2fºF = %.2fºK\n", C, F, K);
-        break;
+                printf("\n%.1fºC = %.1fºF = %.1fºK\n", C, F, K);
+            break;
 
-        case 'f':
-            printf("%c", escolha);
-            
-            F = temp;
-            
-            C = (5 * F - 160) / 9;
-            
-            
-        break;
+            case 2:
+                F = temp;
 
-        case 'k':
-            printf("%c", escolha);
-        break;
+                C = (5 * F - 160) / 9;
+                K = C + 273;
 
-        default:
-            printf("Opção inválida!");
-        break;
-    }
+                printf("\n%.1fºF = %.1fºC = %.1fºK\n", F, C, K);
+            break;
+
+            case 3:
+                K = temp;
+
+                C = K - 273;
+                F = (9 * C + 160) / 5;
+
+                printf("\n%.1fºK = %.1fºF = %.1fºC\n", K, F, C);
+            break;
+
+            default:
+                printf("\nOpção invalida!\n");
+            break;
+        }
+        printf("\n----------------------------------------------");
+        printf("\n\nDeseja tentar novamente?\n");
+        printf("\n(1) - Sim \n(0) - Não\n");
+        scanf("%d", &continuar);
+
+    }while(continuar);
 }
+
+
+
+
+
