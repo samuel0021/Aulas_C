d62aa884ebbde951724f48849eb7fac9bf4e9f78
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-09-11 10:31:29 -0300

    Update inverte_string.c

--- inverte_string.c
+++ inverte_string.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
 #include <conio.h>
 #include <locale.h>
+#include <string.h>
+
+#define TAM 20
 
 int main(){
     setlocale(LC_ALL, "");
 
-    char palavra[6] = "samuel";
-    char inverso[6];
-    int j = 0;
+    char word[TAM];
+    int lengthWord; /*= sizeof(word) / sizeof(word[0]);*/
+    //char invert[];
+    //int lengthInvert = sizeof(invert) / sizeof(invert[0]);
+    //int j = 0;
 
-    for(int i = 6; i >= 0; i--){
-        inverso[j] = palavra[i];
-        j++;
+    printf("\nDigite uma palavra para inverter: ");
+    //scanf("%s", &word);
+    //scanf("%[^\n]", word);
+    gets(word);
+
+    printf("\n\nPalavra digitada: %s\n\n", word);
+    printf("Palavra invertida: ");
+
+    //lengthWord = sizeof(word) / sizeof(word[0]);
+    lengthWord = strlen(word);
+
+    for(int i = lengthWord - 1; i >= 0; i--){
+        //invert[j] = word[i];
+        printf("%c", word[i]);
+
+        //j++;
     }
 
-    printf("\n%s\n", inverso);
+    printf("\n");
+
+    //printf("\n %s ", invert);
+
+    /*for(int k = 0; k <= lengthInvert; k++){
+        printf("\n");
+        printf("%c", invert[k]);
+        printf("\n");
+    }*/
+
+    return 0;
 }
