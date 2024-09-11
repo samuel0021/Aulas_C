1d04d8130b6785778f67baab38a672d48678fb48
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-09-11 20:21:02 -0300

    Create remove_duplicata.c

---
+++ remove_duplicata.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "");
+
+    int numeros[10] = {1, 2, 3, 4, 4, 5, 2, 6, 5};
+    int length = sizeof(numeros) / sizeof(numeros[0]);
+    
+    
+}
