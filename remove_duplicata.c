c0fb190e83a5fe97aa48086650bcdbad08245b7a
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-09-12 11:26:33 -0300

    Update remove_duplicata.c

--- remove_duplicata.c
+++ remove_duplicata.c
@@ -5,8 +5,34 @@
 int main(){
     setlocale(LC_ALL, "");
 
-    int numeros[10] = {1, 2, 3, 4, 4, 5, 2, 6, 5};
-    int length = sizeof(numeros) / sizeof(numeros[0]);
-    
-    
+    int nums[] = {1, 2, 3, 4, 4, 5, 2, 6, 5};
+    int length = sizeof(nums) / sizeof(nums[0]);
+    int numsUnicos[length];
+    int lengthUnicos = 0;
+    int duplicata;
+
+    printf("\n\nArray com duplicatas: ");
+    for(int i = 0; i < length; i++){
+        printf("%d ", nums[i]);
+    }
+
+    for(int i = 0; i < length; i++){
+        duplicata = 0;
+
+        for(int j = 0; j < lengthUnicos; j++){
+            if(nums[i] == numsUnicos[j]){
+                duplicata = 1;
+                break;
+            }
+        }
+        if(!duplicata){
+            numsUnicos[lengthUnicos] = nums[i];
+            lengthUnicos++;
+        }
+    }
+    printf("\n\nArray sem duplicatas: ");
+    for(int i = 0; i < lengthUnicos; i++){
+        printf("%d ", numsUnicos[i]);
+    }
+    printf("\n\n");
 }
