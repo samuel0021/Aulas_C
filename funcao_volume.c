d8569d4204b964f5a6b9e243b7557b1b63fb376e
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-10-08 21:22:10 -0300

    Create funcao_volume.c

---
+++ funcao_volume.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "");
+
+    float v, a;
+
+    printf("\nDigite o valor da aresta do cubo: ");
+    scanf("%f", &a);
+
+    float calcVolume(float);
+
+    v = calcVolume(a);
+
+    printf("\n\nVolume do cubo = %f\n", v);
+
+}
+
+float calcVolume(float a){
+    float v;
+
+    v = pow(a, 3);
+
+    return(v);
+}
