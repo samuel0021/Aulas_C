ca03a5d4c6aa13259c4fd81acb881d9700a550fb
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-10-08 21:38:29 -0300

    Create funcaoCalculadora.c

---
+++ funcaoCalculadora.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+#include <C:\Users\UNIP\Documents\function.h>
+
+int main(){
+    setlocale(LC_ALL, "");
+    
+    float n1, n2;
+
+    printf("\n\t - Calculadora - ");
+    
+    printf("\n\nDigite o 1º número: ");
+    scanf("%f", &n1);
+    
+    printf("\nDigite o 2º número: ");
+    scanf("%f", &n2);
+    
+    printf("\nEscolha a operação: ");
+    printf("\n\t 1 - (+) ")
+}
+
